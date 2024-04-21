08818b3fb7dff77d3860e1e4ebe7f9f688092f38
sek41 <pepin198@hotmaile.comm>
2024-04-21 12:08:29 +0200

    1.2

---
+++ Nueva carpeta (2)/boton/boton/app.js
@@ -0,0 +1,29 @@
+/*function color(){
+    let number= Math.floor(Math.random()*10)
+    switch(number) {
+        case 0:
+          return "red";
+        case 1:
+          return "orange";
+        case 2:
+          return "yellow";
+        case 3:
+          return "green";
+        case 4:
+          return "blue";
+        case 5:
+          return "purple";
+        case 6:
+          return "pink";
+        case 7:
+          return "cyan";
+        case 8:
+          return "magenta";
+        case 9:
+          return "brown";
+        default:
+          alert("Please enter a number between 0 and 9");
+      }
+
+}
+document.getElementById('button').onClick(Document.body.style.backgroundcolor=color())*/
